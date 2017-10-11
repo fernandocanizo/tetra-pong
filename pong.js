a5a29bfc976c3348aeee032d8ffe7a35daa58dd5
Fernando L. Canizo <fernandocanizo@gmail.com>
2017-10-10 21:33:25 -0300

    Make `context` and `playGround` global

--- pong.js
+++ pong.js
@@ -4,6 +4,9 @@ const framesPerSecond = 30;
 const defaultWidth = 800;
 const defaultHeight = 600;
 
+let playGround = null;
+let context = null;
+
 const keydown = () => {};
 
 const dot = {
@@ -36,7 +39,6 @@ const playerOne = Object.assign({}, pad, {
   side: 'left',
   pos: {
     x: 0,
-    // TODO maybe I should make `context` global
     y: Math.floor(Math.random() * defaultHeight),
   },
 });
@@ -55,7 +57,7 @@ const playerTwo = Object.assign({}, pad, {
 
 const middle = (width, height) => ({ x: width / 2, y: height / 2 });
 
-const xPosBySide = (context, pad) => 'left' === pad.side ? 0 : context.canvas.width;
+const xPosBySide = (pad) => 'left' === pad.side ? 0 : context.canvas.width;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Movement functions
@@ -78,7 +80,7 @@ const moveEverything = () => {
 
 const drawNet = () => {};
 
-const drawBackground = ({ context, width, height }) => {
+const drawBackground = () => {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   context.fillStyle = 'black';
   context.fillRect(0, 0, context.canvas.width, context.canvas.height);
@@ -86,7 +88,7 @@ const drawBackground = ({ context, width, height }) => {
 
 const drawScore = () => {};
 
-const drawBall = (context, ball) => {
+const drawBall = (ball) => {
   context.beginPath();
   context.arc(ball.pos.x, ball.pos.y, ball.radius, 0, 2 * Math.PI, false);
   context.fillStyle = ball.color;
@@ -96,31 +98,31 @@ const drawBall = (context, ball) => {
   context.stroke();
 };
 
-const drawPad = (context, pad) => {
+const drawPad = (pad) => {
   context.fillStyle = pad.color;
-  context.fillRect(xPosBySide(context, pad), pad.pos.y, pad.size.x, pad.size.y);
+  context.fillRect(xPosBySide(pad), pad.pos.y, pad.size.x, pad.size.y);
 };
 
 const drawLeftPad = () => {};
 
 const drawRightPad = () => {};
 
-const drawEverything = (context) => {
-  drawBackground({ context, width: context.canvas.width, height: context.canvas.height });
-  drawBall(context, ball);
-  drawPad(context, playerOne);
-  drawPad(context, playerTwo);
+const drawEverything = () => {
+  drawBackground();
+  drawBall(ball);
+  drawPad(playerOne);
+  drawPad(playerTwo);
 };
 
-const updateCanvasSize = (canvas) => {
+const updateCanvasSize = () => {
   // look up the size the canvas is being displayed
   const width = Math.min(document.body.clientWidth, window.innerWidth || defaultWidth);
   const height = Math.min(document.body.clientHeight, window.innerHeight || defaultHeight);
 
   // If it's resolution does not match change it
-  if (canvas.width !== width || canvas.height !== height) {
-    canvas.width = width;
-    canvas.height = height;
+  if (playGround.width !== width || playGround.height !== height) {
+    playGround.width = width;
+    playGround.height = height;
     return true;
   }
 
@@ -132,17 +134,17 @@ const updateCanvasSize = (canvas) => {
 // Main
 ////////////////////////////////////////////////////////////////////////////////
 
-const game = ({ context }) => {
-  updateCanvasSize(context.canvas);
+const game = () => {
+  updateCanvasSize();
   moveEverything();
-  drawEverything(context);
+  drawEverything();
 };
 
 const main = () => {
-  const playGround = document.getElementById('playGround');
-  const context = playGround.getContext('2d');
+  playGround = document.getElementById('playGround');
+  context = playGround.getContext('2d');
   document.addEventListener('keydown', keydown);
-  setInterval(() => game({ context }), 1000 / framesPerSecond);
+  setInterval(() => game(), 1000 / framesPerSecond);
 };
 
 
